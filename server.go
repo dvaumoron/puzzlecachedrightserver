55e633ee5476e3606407417145cc7f49fd1adaf1
dvaumoron <dvaumoron@gmail.com>
2023-01-06 17:20:18 +0100

    stub

---
+++ server.go
@@ -0,0 +1,111 @@
+/*
+ *
+ * Copyright 2022 puzzlecachedrightserver authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+
+	pb "github.com/dvaumoron/puzzlerightservice"
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+)
+
+// server is used to implement puzzlerightservice.RightServer.
+type server struct {
+	pb.UnimplementedRightServer
+	rightServiceAddr string
+	rdb              *redis.Client
+	dataTimeout      time.Duration
+}
+
+func (s *server) AuthQuery(ctx context.Context, request *pb.RightRequest) (*pb.Response, error) {
+	var response *pb.Response
+	var err error
+	return response, err
+}
+
+func (s *server) ListRoles(ctx context.Context, request *pb.ObjectIds) (*pb.Roles, error) {
+	var response *pb.Roles
+	var err error
+	return response, err
+}
+
+func (s *server) RoleRight(ctx context.Context, request *pb.RoleRequest) (*pb.Actions, error) {
+	var actions *pb.Actions
+	var err error
+	return actions, err
+}
+
+func (s *server) UpdateUser(ctx context.Context, request *pb.UserRight) (*pb.Response, error) {
+	return &pb.Response{Success: false}, nil
+}
+
+func (s *server) UpdateRole(ctx context.Context, request *pb.Role) (*pb.Response, error) {
+	return &pb.Response{Success: false}, nil
+}
+
+func (s *server) ListUserRoles(ctx context.Context, request *pb.UserId) (*pb.Roles, error) {
+	var roles *pb.Roles
+	var err error
+	return roles, err
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load .env file")
+	}
+
+	dataTimeoutSec, err := strconv.ParseInt(os.Getenv("UNUSED_DATA_TIMEOUT"), 10, 64)
+	if err != nil {
+		log.Fatal("Failed to parse UNUSED_DATA_TIMEOUT")
+	}
+	dataTimeout := time.Duration(dataTimeoutSec) * time.Second
+
+	dbNum, err := strconv.Atoi(os.Getenv("REDIS_SERVER_DB"))
+	if err != nil {
+		log.Fatal("Failed to parse REDIS_SERVER_DB")
+	}
+
+	lis, err := net.Listen("tcp", ":"+os.Getenv("SERVICE_PORT"))
+	if err != nil {
+		log.Fatalf("Failed to listen : %v", err)
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_SERVER_ADDR"),
+		Username: os.Getenv("REDIS_SERVER_USERNAME"),
+		Password: os.Getenv("REDIS_SERVER_PASSWORD"),
+		DB:       dbNum,
+	})
+
+	s := grpc.NewServer()
+	pb.RegisterRightServer(s, &server{
+		rightServiceAddr: os.Getenv("RIGHT_SERVICE_ADDR"), rdb: rdb, dataTimeout: dataTimeout},
+	)
+	log.Printf("Listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve : %v", err)
+	}
+}
